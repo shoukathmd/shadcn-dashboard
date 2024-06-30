a1f54e10ede08dc1fb9b35aac8f57dc19101b160
shoukathmd <shoukathmd@gmail.com>
2024-06-30 16:32:22 +0530

    Dynamic route page fix

--- src/app/next-openai/drinks/[id]/page.tsx
+++ src/app/next-openai/drinks/[id]/page.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
-const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 import drinkImg from "./drink.jpg";
 import Image from "next/image";
-const getSingleDrink = async (id) => {
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+interface DrinkData {
+  drinks: {
+    strDrink: string;
+    strDrinkThumb: string;
+  }[];
+}
+
+const getSingleDrink = async (id: string): Promise<DrinkData> => {
   const res = await fetch(`${url}${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch a drink...");
@@ -10,7 +19,13 @@ const getSingleDrink = async (id) => {
   return res.json();
 };
 
-const SingleDrinkPage = async ({ params }) => {
+interface SingleDrinkPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const SingleDrinkPage = async ({ params }: SingleDrinkPageProps) => {
   const data = await getSingleDrink(params.id);
   const title = data?.drinks[0]?.strDrink;
   const imgSrc = data?.drinks[0]?.strDrinkThumb;
@@ -18,7 +33,7 @@ const SingleDrinkPage = async ({ params }) => {
   return (
     <div>
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
-        back to drinks
+        Back to drinks
       </Link>
 
       <Image
@@ -34,4 +49,5 @@ const SingleDrinkPage = async ({ params }) => {
     </div>
   );
 };
+
 export default SingleDrinkPage;
