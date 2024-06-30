270f90a91ba907f59cf5c749b2528f300595bfe8
shoukathmd <shoukathmd@gmail.com>
2024-06-30 15:41:07 +0530

    Image issue fix

--- src/components/DrinksList.tsx
+++ src/components/DrinksList.tsx
@@ -1,33 +1,30 @@
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-
-export default function DrinksList({ drinks }: any) {
+import Image from "next/image";
+const DrinksList = ({ drinks }) => {
   return (
-    <div>
-      <ul>
-        {drinks.map((drink: any) => {
-          return (
-            <li key={drink.idDrink}>
-              <Link
-                href={`/drinks/${drink.idDrink}`}
-                className="text-xl font-medium"
-              >
-                <div>
-                  <Image
-                    src={drink.strDrinkThumb}
-                    fill
-                    sizes="(max-width: 768px) 100vw, (max-width:1200px) 50vw"
-                    alt={drink.strDrink}
-                    className="rounded-md object-cover"
-                  />
-                </div>
-                {drink.strDrink}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6">
+      {drinks.map((drink) => {
+        return (
+          <li key={drink.idDrink}>
+            <Link
+              href={`/drinks/${drink.idDrink}`}
+              className="text-xl font-medium"
+            >
+              <div className="relative h-48 mb-4">
+                <Image
+                  src={drink.strDrinkThumb}
+                  fill
+                  sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw"
+                  alt={drink.strDrink}
+                  className="rounded-md object-cover"
+                />
+              </div>
+              {drink.strDrink}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
-}
+};
+export default DrinksList;
